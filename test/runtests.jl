2c1e2ad7ecd3f5ec9eb6f4a813bb0b15ec0bb9c3
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-10-02 14:51:52 -0300

    Add initial tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using RankAggregation
+using DataFrames
 using Test
 
 @testset "RankAggregation.jl" begin
-    # Write your own tests here.
+  objects = DataFrame(object=[:a,:b,:c], score1=[0.9, 0.7, 0.5], score2=[0.8, 0.9, 0.4])
+  @test rank(objects, (:score1,)) == [1,2,3]
+  @test rank(objects, (:score2,)) == [2,1,3]
+  @test rank(objects, (:score1,:score2)) == [1,2,3]
+  @test rank(objects, (:score2,), rev=true) == [2,3,1]
 end
