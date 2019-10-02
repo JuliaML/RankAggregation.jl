c183768c06d5167af137cce26689d9a1fac1e8fa
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-10-02 14:53:43 -0300

    Refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using DataFrames
 using Test
 
 @testset "RankAggregation.jl" begin
-  objects = DataFrame(object=[:a,:b,:c], score1=[0.9, 0.7, 0.5], score2=[0.8, 0.9, 0.4])
+  objects = DataFrame(object=[:a,:b,:c],
+                      score1=[0.9, 0.7, 0.5],
+                      score2=[0.8, 0.9, 0.4])
   @test rank(objects, (:score1,)) == [1,2,3]
   @test rank(objects, (:score2,)) == [2,1,3]
   @test rank(objects, (:score1,:score2)) == [1,2,3]
